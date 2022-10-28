8726cb44b46f0447a14d71cf7ab140d358d04bb8
shinchacoffee <shinchaitstuff@gmail.com>
2022-10-28 20:14:14 +0200

    added class checking mail

---
+++ check_mail.py
@@ -0,0 +1,11 @@
+import re
+
+class MailChecker:
+
+    def __init__(self, address = "a@a.a"):
+        self.mail_templ = r'\b[\w._+-]+@[\w.-]+\.[A-Za-z]{2,3}\b' #my version of regexp
+                         #r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' found on internet... : <
+        self.state = re.fullmatch(self.mail_templ, address)
+        return self.state
+
+    #got this well known feeling that i should not write it like that... : <
