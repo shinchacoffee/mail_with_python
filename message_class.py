22217734944279724d6fed086e14d38ef241cbc8
shinchacoffee <shinchaitstuff@gmail.com>
2022-11-03 23:33:14 +0100

    created message class but not functionall at this moment

---
+++ message_class.py
@@ -0,0 +1,21 @@
+import user_class
+#import content_class
+#import attachment_class
+#import addressee_class
+#import sender_class
+
+class Message:
+
+    def __init__(self):
+        self.senderMail = "nonusablemailacc@gmail.com"
+        self.senderPass = "passwordfornonusbalemailacc" #should be somhowe encrypted... or at least not in git?
+        self.receiverMail = "nonusablemailacc@gmail.com"
+        self.content = "Please phone me as soon as possible."
+        self.attachment = None
+        self.status = "not sent"
+
+    def constructMail(self):
+        self.status = "constructed"
+
+    def sendMail(self):
+        self.status = "mail sent successfully"
