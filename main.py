93ecd88c2b5259a128a2a6a9b9d088e9443c6a0d
shinchacoffee <shinchaitstuff@gmail.com>
2022-11-03 23:33:30 +0100

    added main file with import of message class and call

---
+++ main.py
@@ -0,0 +1,12 @@
+import message_class
+#import user_class
+#import content_class
+#import attachment_class
+#import addressee_class
+#import sender_class
+
+my_new_message = Message()
+
+my_new_message.constructMail()
+
+my_new_message.sendMail()
