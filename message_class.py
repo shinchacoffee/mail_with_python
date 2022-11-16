f00ee5cc82ba702bc6f8d1c08c8b35f9f583ddf9
shinchacoffee <shinchaitstuff@gmail.com>
2022-11-16 01:11:33 +0100

    mail checker class used in user_class and in message_class

--- message_class.py
+++ message_class.py
@@ -1,4 +1,5 @@
-#import user_class
+import user_class
+import check_mail
 #import content_class
 #import attachment_class
 #import addressee_class
@@ -9,10 +10,17 @@ class Message:
     def __init__(self, mail_order):
         self.senderMail = "nonusablemailacc@gmail.com"
         self.senderPass = "passwordfornonusbalemailacc" #should be somehow encrypted... or at least not in a git?
-        self.receiverMail = mail_order
-        self.content = "Please phone me as soon as possible."
-        self.attachment = None
-        self.status = "not sent"
+        self.is_it_only_mail = check_mail.MailChecker(mail_order)
+        if self.is_it_only_mail:
+            self.receiverMail = mail_order
+            self.content = "Please phone me as soon as possible."
+            self.attachment = None
+            self.status = "ready to send"
+        else:
+            self.receiverMail = None
+            self.content = None
+            self.attachment = None
+            self.status = "custom order"
 
     def constructMail(self):
         self.status = "constructed"
--- user_class.py
+++ user_class.py
@@ -1,4 +1,5 @@
 import re
+import check_mail
 class User:
 
     def __init__(self):
@@ -7,16 +8,14 @@ class User:
         self.addr_state, self.pass_state = "default_mail", "default_pass"
 
     def get_address(self):
-        #string = user_input()
         string = input('Please enter your e-mail address: ')
-        mail_templ = r'\b[\w._+-]+@[\w.-]+\.[A-Za-z]{2,3}\b' #my version of regexp
-                    #r'\b[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Z|a-z]{2,}\b' found on internet... : <
-        if re.fullmatch(mail_templ, string):
+        is_it_proper_mail = check_mail.MailChecker(string)
+        if is_it_proper_mail.state:
             address = string
             address_state = "ok"
         else:
-            print("Invalid e-mail address. Address set to asd@asd.asd")
-            address = "asd@asd.asd"
+            print("Invalid e-mail address. Address set to None")
+            address = None
             address_state = "wrong mail"
         return address, address_state
 
